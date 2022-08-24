6108c591a234459175d87523409541bbd7c7ea04
Thales Sousa <thalestjsb@gmail.com>
2022-08-24 16:21:01 -0300

    0808 useForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,32 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Input } from '../Forms/Input'
-import { Button } from '../Forms/Button'
+import { Input } from "../Forms/Input";
+import { Button } from "../Forms/Button";
+import { useForm } from "../../Hooks/useForm";
 
 export function LoginForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const username = useForm();
+  const password = useForm();
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const response = await fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({ username, password })
-    })
-
-    const json = await response.json()
-
-    console.log(json)
+    if (username.validate() && password.validate()) {
+      const response = await fetch(
+        "https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            username: username.value,
+            password: password.value,
+          }),
+        }
+      );
+      const json = await response.json();
+      console.log(json);
+    }
   }
 
   return (
     <section onSubmit={handleSubmit}>
       <h1>Login</h1>
       <form>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
