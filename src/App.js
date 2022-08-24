d227b3b1d416cbfa948bdccc9277692994f8a538
Thales Sousa <thalestjsb@gmail.com>
2022-08-24 12:12:38 -0300

    0804 Estrutura Inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { Api } from './api/Api'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Footer } from './Components/Footer'
+import { Header } from './Components/Header'
+import { Home } from './Components/Home'
+import { Login } from './Components/Login/Login'
 
 export default function App() {
   return (
-    <div>
-      <Api />
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
