2000627e4430b8669a64e92f64ff744e369e7e68
Thales Sousa <thalestjsb@gmail.com>
2022-08-25 16:47:37 -0300

    0814 Login Criar

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const types = {
   email: {
     regex:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: "Preencha um email válido",
+    message: 'Preencha um email válido',
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
   },
 };
 
 export function useForm(type) {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   const [error, setError] = React.useState(null);
 
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError("Preencha um valor.");
+      setError('Preencha um valor.');
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
@@ -27,7 +32,7 @@ export function useForm(type) {
   }
 
   function onChange({ target }) {
-    if (error) validate(target.value)
+    if (error) validate(target.value);
     setValue(target.value);
   }
   return {
