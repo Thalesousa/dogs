47dfadc3070a043d76054eccc728e786da19e32b
Thales Sousa <thalestjsb@gmail.com>
2022-09-19 00:30:07 -0300

    0825 Image Skeleton

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -4,6 +4,7 @@ import styles from './PhotoContent.module.css';
 import { PhotoComments } from './PhotoComments';
 import { UserContext } from '../../Context/UserContext';
 import PhotoDelete from './PhotoDelete';
+import { Image } from '../Helper/Image';
 
 export function PhotoContent({ data }) {
   const user = useContext(UserContext);
@@ -11,7 +12,7 @@ export function PhotoContent({ data }) {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
