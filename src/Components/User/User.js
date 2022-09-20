3e8b3f5a25bfeab0564055043163ce8ec2f65bc3
Thales Sousa <thalestjsb@gmail.com>
2022-09-19 23:27:41 -0300

    0826 Loading

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { UserHeader } from './UserHeader';
-import { Feed } from '../Feed/Feed';
 import { UserPhotoPost } from './UserPhotoPost';
 import { UserStats } from './UserStats';
+import { Home } from '../Home';
 
 export function User({ children }) {
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Home />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatistica" element={<UserStats />} />
       </Routes>
